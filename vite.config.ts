43343afd4a6cbdc00e6e5e279fd22f29e5881e2e
SnehaMS325 <majumdersneha325@email.com>
2026-08-05 09:58:08 +0530

    viteconfig edited

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 
 export default defineConfig(({ mode }) => ({
-  base: "/SnehaMajumder-Portfolio/", 
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
